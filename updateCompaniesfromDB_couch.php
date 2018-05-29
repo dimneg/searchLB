6ca6e542be2dc503f29cd5c79e168c9ae4da91a6
dimitris negkas <dimneg@gmail.com>
2018-05-29 12:22:18 +0300

    id

--- updateCompaniesfromDB_couch.php
+++ updateCompaniesfromDB_couch.php
@@ -13,7 +13,7 @@ $db = FRcouchDB;
 $ch = curl_init();
 
 #$sql = "SELECT * FROM Main where orgtype <> 'FR'  and issueddate >= '$dateUpdate'  limit 10000 offset 10000";
-$sql = "SELECT * FROM Main where orgtype = 'FR'  limit 50000 offset 50000";
+$sql = "SELECT * FROM Main where orgtype = 'FR'  limit 50000 offset 0";
 
 $result = $connGemh->query($sql);
 if ($result->num_rows > 0) {
