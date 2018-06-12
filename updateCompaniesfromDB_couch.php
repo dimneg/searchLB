11da00fe9666cc784b4caf1db8cd97e2443c1cba
dimitris negkas <dimneg@gmail.com>
2018-06-12 19:31:52 +0300

    update

--- updateCompaniesfromDB_couch.php
+++ updateCompaniesfromDB_couch.php
@@ -5,16 +5,31 @@ include 'collectData.php';
 $time_pre = microtime(true);
 $counter = 1;
 $transform = new collectData();
-$dateUpdate = '1900-01-01';
+$dateUpdate = '2018-05-29';
 $connGemh =  new MySQLi(gemhDb_host, gemhDb_user, gemhDb_pass, gemhDb_name);
 mysqli_set_charset($connGemh,"utf8");
 
 $db = FRcouchDB;
 $ch = curl_init();
 
-#$sql = "SELECT * FROM Main where orgtype <> 'FR'  and issueddate >= '$dateUpdate'  limit 10000 offset 10000";
-$sql = "SELECT * FROM Main where orgtype = 'FR' ";
+###settings for update######
+ #$dir_base= '/var/www/linkedeconomy/mupl/AutoOutput'; 
+ $dir_base= "d:/temp/fr/";
 
+ if (file_exists($dir_base."found/")) {
+     deleteDir($dir_base."found/"); //delete temp folder
+     if (!is_dir_empty($dir_base."found/")){
+         mkdir($dir_base."found/", 0777); //create temp folder for files to be updated
+     }
+         
+ }
+
+######################
+
+
+#$sql = "SELECT * FROM Main where orgtype <> 'FR'  and issueddate >= '$dateUpdate'  limit 10000 offset 10000";
+$sql = "SELECT * FROM Main where orgtype = 'FR' and issueddate >= '$dateUpdate ' limit 890 offset 110";
+echo $sql.PHP_EOL;
 $result = $connGemh->query($sql);
 if ($result->num_rows > 0) {
      while($row = $result->fetch_assoc()){
@@ -34,12 +49,51 @@ if ($result->num_rows > 0) {
              # $id = $row['gemhnumber'].'-'.$row['vatId'];
               $link = $row['vatId'].'-'.$row['gemhnumber'];
          }
-          $id = $row['gemhnumber'];
+         $id = $row['gemhnumber'];
          echo $row['orgType'].' '.$id;
        # $ch = curl_init("http://83.212.86.164:8983/solr/".$core."/update?wt=json");
+       # 
+         //find and delete
+         $chUpd = curl_init();
+         $urlUpd = couchPath.$db.'/'.$id;
+         echo $urlUpd.PHP_EOL;
+         curl_setopt($chUpd, CURLOPT_URL, $urlUpd); 
+	 curl_setopt( $chUpd, CURLOPT_USERPWD, 'dimneg:dim1978');	
+         curl_setopt( $chUpd, CURLOPT_CUSTOMREQUEST, 'GET');		
+	 curl_setopt( $chUpd, CURLOPT_RETURNTRANSFER, true);		
+	 $resultUpd = curl_exec($chUpd);
+         if(!curl_errno($chUpd)){             
+              $info = curl_getinfo($chUpd);
+              #echo 'Took ' . $info['total_time'] . ' seconds to send a request (get) to ' . $info['url'].'<br>';
+         }
+         curl_close( $chUpd );
+         #print_r($info);
+         if ($info['http_code']===200){
+             $destination =$dir_base."found/".str_replace('%3F', '?', $id).'.json';
+             $file = fopen($destination, "w+");
+	     fputs($file, $resultUpd);
+	     fclose($file);
+             
+             $fdel=file_get_contents( $destination );
+             $jsonDel=json_decode( $fdel,true);
+	     $jsonDel['_id']=str_replace('/', '%2F', $jsonDel['_id']);
+	     $jsonDel['_id'] = str_replace('?','%3F', $jsonDel['_id']);
+	     $jsonDel['_id'] = str_replace('?','%3F', $jsonDel['_id']);
+	     $urlDel=couchPath.$db.'/'.$jsonDel['_id'];	
+             
+             echo  $urlDel.' will be updated'.PHP_EOL;
+             
+             $urlrev=$urlDel.'?rev='.$jsonDel['_rev'];
+             $chDel = curl_init();
+             curl_setopt($chDel , CURLOPT_URL, $urlrev); 
+	     curl_setopt($chDel, CURLOPT_USERPWD, 'dimneg:dim1978');          
+             curl_setopt($chDel, CURLOPT_CUSTOMREQUEST, 'DELETE');
+             curl_setopt($chDel, CURLOPT_RETURNTRANSFER, true);
+             $resultDel = curl_exec( $chDel);
+             curl_close($chDel);
+         }
          
-      
-                 $arr = array(
+         $arr = array(
                     "id"   => $id,
                     "vat"   => $row['vatId'],     
                     "gemhNumber"   => $row['gemhnumber'],     
@@ -67,7 +121,7 @@ if ($result->num_rows > 0) {
          $file_contents=json_encode($arr,JSON_UNESCAPED_UNICODE);
 
         # curl_setopt($ch, CURLOPT_URL, 'localhost:5984/'.$db.'/'.$id);
-         curl_setopt($ch, CURLOPT_URL, 'http://83.212.86.158:5984/'.$db.'/'.$id);
+         curl_setopt($ch, CURLOPT_URL, couchPath.$db.'/'.$id);
          curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT'); /* or PUT */
          curl_setopt($ch, CURLOPT_POSTFIELDS, $file_contents);
          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -106,5 +160,31 @@ echo '(In '.number_format($exec_time/60,2).' mins)'.PHP_EOL ;
 ############### delete all json from core
 # sudo curl "http://127.0.0.1:8983/solr/LbPersons/update?commit=true" -H "Content-Type: text/xml" --data-binary '<delete><query>*:*</query></delete>'
 
-
+function deleteDir($dirPath) {
+    if (! is_dir($dirPath)) {
+        throw new InvalidArgumentException("$dirPath must be a directory");
+    }
+    if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+        $dirPath .= '/';
+    }
+    $files = glob($dirPath . '*', GLOB_MARK);
+    foreach ($files as $file) {
+        if (is_dir($file)) {
+            self::deleteDir($file);
+        } else {
+            unlink($file);
+        }
+    }
+    rmdir($dirPath);
+}
+function is_dir_empty($dir) {
+  if (!is_readable($dir)) return NULL; 
+  $handle = opendir($dir);
+  while (false !== ($entry = readdir($handle))) {
+    if ($entry != "." && $entry != "..") {
+      return FALSE;
+    }
+  }
+  return TRUE;
+} 
 
